dcec9a9ba1f0c47ff438d7530c90bf5b4eaf44c1
alex <desorbaix@free.fr>
2020-12-16 13:59:06 +0100

    decalaration d'une fonction + appel

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-//deffinition d'une variable et affectation d'un contenu
-var jsload=document.querySelector('#jsload');
-//moddif. du contenu html de la balise dans la var
-jsload.innerHTML='mon <span style="font-weight:900">JS</span> est chargé';
-//moddif. du style de la balise dans la var
-jsload.style.backgroundColor='LIME';
+//declaration d'une fonction
+function initialisationJS(prenom) {
+    //deffinition d'une variable et affectation d'un contenu
+    var jsload = document.querySelector('#jsload');
+    //moddif. du contenu html de la balise dans la var
+    jsload.innerHTML = 'mon <span style="font-weight:900">JS</span> est chargé pour '+prenom;
+    //moddif. du style de la balise dans la var
+    jsload.style.backgroundColor = 'LIME';
+}
+//usage d'une fonction
+initialisationJS('Alexandre');
